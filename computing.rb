fc9b99750b71251f43c2d5a277026b7b18f31aac
tompng <tomoyapenguin@gmail.com>
2016-06-11 15:45:07 +0900

    memset bugfix

--- computing.rb
+++ computing.rb
@@ -58,12 +58,12 @@ class Computer
   end
 
   module Internal
-    def self.command_data command, powered: false
+    def self.command_data command, redstone: false
       MCWorld::Tag::Hash.new(
         'conditionMet' => MCWorld::Tag::Byte.new(0),
-        'auto' => MCWorld::Tag::Byte.new(0),
+        'auto' => MCWorld::Tag::Byte.new(redstone ? 0 : 1),
         'customName' => MCWorld::Tag::String.new('@'),
-        'powered' => MCWorld::Tag::Byte.new(powered ? 1 : 0),
+        'powered' => MCWorld::Tag::Byte.new(0),
         'Command' => MCWorld::Tag::String.new(command),
         'id' => MCWorld::Tag::String.new('Control'),
         'SuccessCount' => MCWorld::Tag::Int.new(0),
@@ -77,37 +77,41 @@ class Computer
       x, y, z = MEM_REF[:x], MEM_REF[:y], MEM_REF[:z]
       memz = MEM_ADDRESS[:z]
       vx, vy, vz = MEM_VALUE[:x], MEM_VALUE[:y], MEM_VALUE[:z]
-      add = ->(command, chain: false, cond: false){
+      add = ->(command, chain: false, cond: false, redstone: false){
         unless command
           blocks << nil
           next
         end
         block = (chain ? MCWorld::Block::ChainCommandBlock : MCWorld::Block::CommandBlock)
-        data = MCWorld::Block::Data::Z_MINUS | (cond ? 0 : MCWorld::Block::Data::MASK)
-        blocks << [block[data], command_data(command)]
+        data = MCWorld::Block::Data::Z_MINUS | (cond ? MCWorld::Block::Data::MASK : 0)
+        blocks << [block[data], command_data(command, redstone: redstone)]
       }
       add[nil]
       7.times{|i|
-        add["testforblock #{x} #{y} #{z+2*i} stone"]
+        add["testforblock #{x} #{y} #{z+2*i} stone", redstone: true]
         add["clone ~ ~ #{memz-size} ~ ~ #{memz-1} ~#{1<<i} ~ #{memz-size}", chain: true, cond: true]
         add["setblock ~#{1<<i} ~ ~-3 redstone_block", chain: true, cond: true]
         add["fill ~ ~ #{memz-1} ~ ~ #{memz-size} air", chain: true, cond: true]
-        add["setblock ~ ~ ~-3 redstone_block", chain: true]
+        add["setblock ~ ~ ~-1 redstone_block", chain: true]
         add[nil]
-        add["testforblock #{x} #{y} #{z+2*i+1} stone"]
+        add["testforblock #{x} #{y} #{z+2*i+1} stone", redstone: true]
         add["clone ~ ~ #{memz-size} ~ ~ #{memz-1} ~ ~#{1<<i} #{memz-size}", chain: true, cond: true]
         add["setblock ~ ~#{1<<i} ~-3 redstone_block", chain: true, cond: true]
         add["fill ~ ~ #{memz-1} ~ ~ #{memz-size} air", chain: true, cond: true]
-        add["setblock ~ ~ ~-3 redstone_block", chain: true]
+        add["setblock ~ ~ ~-1 redstone_block", chain: true]
         add[nil]
       }
       4.times{|i|
-        add["testforblock #{x} #{y} #{z+14} #{i%2==1 ? 'stone' : 'air'}", chain: true]
+        if i==0
+          add["testforblock #{x} #{y} #{z+14} #{i%2==1 ? 'stone' : 'air'}", redstone: true]
+        else
+          add["testforblock #{x} #{y} #{z+14} #{i%2==1 ? 'stone' : 'air'}", chain: true]
+        end
         add["testforblock #{x} #{y} #{z+15} #{i/2==1 ? 'stone' : 'air'}", chain: true, cond: true]
         if mode == :get
-          add["clone ~ ~ #{memz+VALUE_BITS*i} ~ ~ ~ #{memz+VALUE_BITS*(i+1)} #{vx} #{vy} #{vz} replace", chain: true, cond: true]
+          add["clone ~ ~ #{memz+VALUE_BITS*i} ~ ~ #{memz+VALUE_BITS*(i+1)} #{vx} #{vy} #{vz} replace", chain: true, cond: true]
         else
-          add["clone #{vx} #{vy} #{vz} #{vx} #{vy} #{vz+VALUE_BITS} ~ #{memz+VALUE_BITS*i} replace", chain: true, cond: true]
+          add["clone #{vx} #{vy} #{vz} #{vx} #{vy} #{vz+VALUE_BITS} ~ ~ #{memz+VALUE_BITS*i} replace", chain: true, cond: true]
         end
       }
       add["setblock #{OP_DONE[:x]} #{OP_DONE[:y]} #{OP_DONE[:z]} redstone_block", chain: true]
@@ -124,15 +128,11 @@ class Computer
       end
     end
     def self.prepare world
-      (128/16).times.to_a.repeated_permutation(3){|i,j,k|
-        world[i*16+MEM_ADDRESS[:x],j*16+MEM_ADDRESS[:y],k*16+MEM_ADDRESS[:z]]=nil
-        world[i*16+15+MEM_ADDRESS[:x],j*16+15+MEM_ADDRESS[:y],k*16+15+MEM_ADDRESS[:z]]=nil
-      }
       [[seek_get_blocks, SEEK_GET], [seek_set_blocks, SEEK_SET]].each do |block_tile_entities, pos|
         block_tile_entities.each_with_index{|bt, i|
           block, tile_entity = bt
-          world[pos[:x],pos[:y],pos[:z]+i] = block
-          world.tile_entities[pos[:x],pos[:y],pos[:z]+i] = tile_entity
+          world[pos[:x],pos[:z]+i,pos[:y]] = block
+          world.tile_entities[pos[:x],pos[:z]+i,pos[:y]] = tile_entity
         }
       end
     end
@@ -152,6 +152,18 @@ class Computer
 end
 
 Computer.new do
+  pos_get = Computer::SEEK_GET
+  pos_set = Computer::SEEK_SET
+  @world[pos_get[:x],pos_get[:z]-2,pos_get[:y]-1]=MCWorld::Block::Stone
+  @world[pos_set[:x],pos_set[:z]-2,pos_get[:y]-1]=MCWorld::Block::Stone
+  @world[pos_get[:x],pos_get[:z]-3,pos_get[:y]] = MCWorld::Block::CommandBlock[MCWorld::Block::Data::Z_MINUS]
+  @world.tile_entities[pos_get[:x],pos_get[:z]-3,pos_get[:y]]=Computer::Internal.command_data mem_op_begin_command(:get), redstone: true
+  @world[pos_get[:x],pos_get[:z]-4,pos_get[:y]] = MCWorld::Block::ChainCommandBlock[MCWorld::Block::Data::Z_MINUS]
+  @world.tile_entities[pos_get[:x],pos_get[:z]-4,pos_get[:y]]=Computer::Internal.command_data mem_op_execute_command
+  @world[pos_set[:x],pos_set[:z]-3,pos_set[:y]] = MCWorld::Block::CommandBlock[MCWorld::Block::Data::Z_MINUS]
+  @world.tile_entities[pos_set[:x],pos_set[:z]-3,pos_set[:y]]=Computer::Internal.command_data mem_op_begin_command(:set), redstone: true
+  @world[pos_set[:x],pos_set[:z]-4,pos_set[:y]] = MCWorld::Block::ChainCommandBlock[MCWorld::Block::Data::Z_MINUS]
+  @world.tile_entities[pos_set[:x],pos_set[:z]-4,pos_set[:y]]=Computer::Internal.command_data mem_op_execute_command
   File.write outfile, @world.encode
 end
 
