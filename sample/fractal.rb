650392d3e1c6cad1c6c9b4fc9e17b70cfc294930
tompng <tomoyapenguin@gmail.com>
2016-06-19 16:23:58 +0900

    fractal with z-dir

--- sample/fractal.rb
+++ sample/fractal.rb
@@ -5,9 +5,10 @@ computer = Computer.new do
   var.shape = 1
   var.data[0] = var.shape
   var.y = 0
+  "Memory 3D Plot".each_char{|c|putc c}
   exec_while var.y < 127 do
     var.x = 0
-    var.shape += var.shape + 1
+    var.shape = var.shape ^ (var.shape + var.shape)
     var.data[var.index+128] = var.shape
     exec_while var.x <= var.y do
       exec_if !!var.data[var.index+var.x] + !!var.data[var.index+var.x+1] == 1 do
