045ac40884ee8893ed3b73fcb55fbad44501d146
tompng <tomoyapenguin@gmail.com>
2016-06-20 02:01:29 +0900

    fractal plot to display

--- sample/fractal.rb
+++ sample/fractal.rb
@@ -2,18 +2,23 @@ require_relative '../computing/computing'
 computer = Computer.new do
   array data: 127*128
   variable :x, :y, :index, :shape
+  "Memory 3D Plot\n".each_char{|c|putc c}
   var.shape = 1
   var.data[0] = var.shape
-  var.y = 0
-  "Memory 3D Plot".each_char{|c|putc c}
+  putc '#'
   exec_while var.y < 127 do
+    putc "\n"
     var.x = 0
     var.shape = var.shape ^ (var.shape + var.shape)
     var.data[var.index+128] = var.shape
+    putc '#'
     exec_while var.x <= var.y do
-      exec_if !!var.data[var.index+var.x] + !!var.data[var.index+var.x+1] == 1 do
+      exec_if(!!var.data[var.index+var.x] + !!var.data[var.index+var.x+1] == 1){
         var.data[var.index+var.x+129] = var.shape
-      end
+        putc '#'
+      }.else{
+        putc ' '
+      }
       var.x += 1
     end
     var.y += 1
